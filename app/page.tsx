2d58697d216d6d2cc4a9e6acc9525f1bc94d85fe
pranavcpawar <pranavcpawar@outlook.com>
2025-06-14 01:27:07 +0530

    add parse page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,152 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormMessage,
+} from "@/components/ui/form";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { parseCode } from "./_actions/code-parser";
+import { useState } from "react";
 
+type CodeFunction = {
+	name: string;
+	type: string;
+	visibility: string;
+	mutability: string;
+	isRead: boolean;
+};
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const [codeFunctions, setCodeFunctions] = useState<CodeFunction[]>([]);
+	const [readFunctions, setReadFunctions] = useState<{ name: string }[]>([]);
+	const [writeFunctions, setWriteFunctions] = useState<{ name: string }[]>([]);
+	const schema = z.object({
+		code: z.string().min(1),
+	});
+	type Schema = z.infer<typeof schema>;
+	const form = useForm<Schema>({
+		resolver: zodResolver(schema),
+		defaultValues: {
+			code: "",
+		},
+	});
+	async function onSubmit(data: Schema) {
+		try {
+			const response = await parseCode(data.code);
+			if (response) {
+				setCodeFunctions(response.codeFunctions);
+				setReadFunctions(response.ReadFunctions);
+				setWriteFunctions(response.WriteFunctions);
+			}
+			form.reset();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	console.log(codeFunctions);
+	return (
+		<div className="flex h-full w-full md:flex-row flex-col items-center justify-center gap-2 p-2">
+			<div className="flex flex-col items-center justify-center gap-2 w-full h-full rounded-xl p-3">
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="w-full h-full flex flex-col gap-4 items-center justify-center"
+					>
+						<FormField
+							control={form.control}
+							name="code"
+							render={({ field }) => (
+								<>
+									<FormControl>
+										<textarea
+											className="w-full h-full border-2 border-border rounded-xl p-2 outline-none min-h-60"
+											placeholder="Paste your code here"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage className="font-work-sans h-4 text-sm font-medium tracking-tight" />
+								</>
+							)}
+						/>
+						<Button
+							className="w-full h-10 bg-[#0e76a8] text-white rounded-2xl hover:bg-[#0e76a8]/80 font-manrope text-base font-bold tracking-tight"
+							type="submit"
+						>
+							Submit
+						</Button>
+					</form>
+				</Form>
+			</div>
+			<div className="flex flex-col items-start justify-center gap-2 border-2 w-full h-full rounded-xl p-2">
+				<h1 className="font-unbounded text-xl font-bold tracking-tight">
+					Stats
+				</h1>
+				<div className="flex flex-col items-start justify-center gap-2 w-full h-full">
+					<div className="flex flex-col items-start justify-center gap-2 w-full">
+						<div className="flex flex-row items-start justify-center gap-2">
+							<p className="font-work-sans h-4 text-sm font-medium tracking-tight">
+								Number of functions used:
+							</p>
+							<p className="font-work-sans h-4 text-sm font-medium tracking-tight text-tertiary">
+								{codeFunctions.length ? codeFunctions.length : "null"}
+							</p>
+						</div>
+						<div className="flex flex-row items-start justify-center gap-2">
+							<p className="font-work-sans h-4 text-sm font-medium tracking-tight">
+								Number of read functions used:
+							</p>
+							<p className="font-work-sans h-4 text-sm font-medium tracking-tight text-secondary">
+								{readFunctions.length ? readFunctions.length : "null"}
+							</p>
+						</div>
+						<div className="flex flex-row items-start justify-center gap-2">
+							<p className="font-work-sans h-4 text-sm font-medium tracking-tight">
+								Number of write functions used:
+							</p>
+							<p className="font-work-sans h-4 text-sm font-medium tracking-tight text-primary">
+								{writeFunctions.length ? writeFunctions.length : "null"}
+							</p>
+						</div>
+					</div>
+					<Table className="w-full border-2 border-border">
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">Function</TableHead>
+								<TableHead>Type</TableHead>
+								<TableHead>Visibility</TableHead>
+								<TableHead className="text-right">Mutability</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{codeFunctions.map((codeFunction) => (
+								<TableRow key={codeFunction.name}>
+									<TableCell className="font-medium">
+										{codeFunction.name}
+									</TableCell>
+									<TableCell>
+										{codeFunction.isRead ? "read" : "write"}
+									</TableCell>
+									<TableCell>{codeFunction.visibility}</TableCell>
+									<TableCell className="text-right">
+										{codeFunction.mutability}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
+			</div>
+		</div>
+	);
 }
